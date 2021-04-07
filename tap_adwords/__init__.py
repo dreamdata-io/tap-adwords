aa5777820bc4e02ee91bbd73ea228c5e03791306
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2021-04-07 16:30:11 +0200

    clear progress state as we go

--- tap_adwords/__init__.py
+++ tap_adwords/__init__.py
@@ -280,6 +280,9 @@ def sync_report(stream: str, field_list: List[str], sdk_client: adwords.AdWordsC
             start_date.strftime(utils.DATETIME_FMT_SAFE),
         )
         singer.write_state(STATE)
+    bookmarks.clear_bookmark(
+        STATE, stream, state_key_name(customer_id, "last_attribution_window_date")
+    )
     singer.write_state(STATE)
     LOGGER.info("Done syncing the %s report for customer_id %s", stream, customer_id)
 
